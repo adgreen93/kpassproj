7ca8ab597f77f8349de50f808cf0162dad16310c
adgreen93 <adgreen93@gmail.com>
2016-09-10 18:03:05 -0400

    finishing touches on city pass

--- index.js
+++ index.js
@@ -11,20 +11,6 @@ app.get('/', function(req, res){
 	res.render('home');
 });
 
-// Load the full build. 
-var _ = require('lodash');
-// Load the core build. 
-var _ = require('lodash/core');
-// Load the fp build for immutable auto-curried iteratee-first data-last methods. 
-var fp = require('lodash/fp');
- 
-// Load a method category. 
-var array = require('lodash/array');
-var object = require('lodash/fp/object');
- 
-// Load a single method for smaller builds with browserify/rollup/webpack. 
-var chunk = require('lodash/chunk');
-var extend = require('lodash/fp/extend');
 
 // STARTS SERVER 
 
--- public/js/app.js
+++ public/js/app.js
@@ -5,12 +5,38 @@ var app = angular.module('kidsApp', []);
 app.controller('mapController2', function PostsCtrlAjax($scope, $http) {
 
     $http.get('js/locationsResponse.json').then(function(response) {
-        $scope.locations = response.data.data;
-   
-        console.log($scope.locations)
-       
- 	$scope.clickAlert = function clickAlert() {
-      alert('it worked!')
+		var values = response.data.data;
+		$scope.cleanmacros = values
+
+	var bucket = [];
+	var macros = [];
+	var insides = [];
+	var hoods = [];
+
+	var a;
+	var b;
+	var c;
+
+	for (a = 0; a < values.length; a++) {
+
+		bucket.push(values[a].borough.name)
+		
+		for (b = 0; b < values[a].mappings.length; b++) {
+			
+			bucket.push(values[a].mappings[b].macro.name)
+
+			for (c = 0; c < values[a].mappings[b].neighborhoods.length; c++) {
+			
+			bucket.push(values[a].mappings[b].neighborhoods[c].name)
+
+			}		
+		}
+	}
+
+	console.log(bucket);
+	       
+ 	$scope.popClock = function popClock() {
+      document.getElementById("boro-level").style.color = "green";
     };
 
     });
