aec852a92ed1b753575afca187a6fd84d6e96ede
adgreen93 <adgreen93@gmail.com>
2016-09-10 18:35:58 -0400

    added lots of comment for readability

--- public/js/app.js
+++ public/js/app.js
@@ -4,10 +4,17 @@ var app = angular.module('kidsApp', []);
 
 app.controller('mapController2', function PostsCtrlAjax($scope, $http) {
 
+
+	// This is where we get the JSON. if this was live, it'd be a real request.
     $http.get('js/locationsResponse.json').then(function(response) {
 		var values = response.data.data;
 		$scope.cleanmacros = values
 
+	//This was an alternative solution that I left in the code - basically it loops through the JSON, pushing borough names, 
+	//macro names, and neighborhood names into an array. the weakness here is that while yes, you get an organized view in the UI
+	//this is all it's really goood for. the angular solution is way cleaner and easier to dig through.
+
+	//so this could be a solution if it was completely vanilla javascript.
 	var bucket = [];
 	var macros = [];
 	var insides = [];
@@ -33,8 +40,12 @@ app.controller('mapController2', function PostsCtrlAjax($scope, $http) {
 		}
 	}
 
+	//this is what that massive array looks like. i had also tested this a few different ways - adding them to different arrays
+	//trying to rebuild the object, but for cleanliness it felt that angular was most effective.
 	console.log(bucket);
-	       
+	 
+	//this is the function that'll change colors in the UI. it's not smart - it's a once a page load thing. continue to tinker or
+	//this and id make it another ng-hide/show most likely.
  	$scope.popClock = function popClock() {
       document.getElementById("boro-level").style.color = "green";
     };
